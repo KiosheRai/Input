1f40a70d56963215a2fa0042ba6c0b6f077f4a20
Adel <prop.het@mail.ru>
2021-07-06 21:51:48 +0300

    Добавлен ворд

--- Input/CustomerShowOrder.xaml.cs
+++ Input/CustomerShowOrder.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace Input
 {
@@ -50,6 +52,46 @@ namespace Input
 
             CustomerPage nextPage = new CustomerPage(Login);
             nav.Navigate(nextPage);
+        }
+
+        private void ReplaceWordStub(String stubToReplace, String Text, Word.Document word)
+        {
+            var range = word.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: stubToReplace, ReplaceWith: Text);
+        }
+
+        private void ReportGo()
+        {
+            DataTable table = SQLbase.Select($"select * from Orders where id = {Id}");
+
+            try
+            {
+                var word = new Word.Application();
+                word.Visible = false;
+
+                var worddoc = word.Documents.Open($"{Environment.CurrentDirectory}/ReportMaket.DOCX");
+
+                ReplaceWordStub("{id}", table.Rows[0][0].ToString(), worddoc);
+                ReplaceWordStub("{customer}", table.Rows[0][1].ToString(), worddoc);
+                ReplaceWordStub("{title}", table.Rows[0][2].ToString(), worddoc);
+                ReplaceWordStub("{description}", table.Rows[0][3].ToString(), worddoc);
+                ReplaceWordStub("{price}", table.Rows[0][4].ToString(), worddoc);
+                ReplaceWordStub("{programmer}", table.Rows[0][6].ToString(), worddoc);
+
+                worddoc.SaveAs2($"{Environment.CurrentDirectory}/report.docx");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Report(object sender, RoutedEventArgs e)
+        {
+            Thread t = new Thread(ReportGo);
+            t.Start();
+            MessageBox.Show("Отчёт создан!");
         }
     }
 }
